b537dccc0dc064c6d48c373cd62845546632b68c
zelalemzergaw <50334133+zelalemzergaw@users.noreply.github.com>
2019-06-11 16:24:00 -0500

    NEW EDITED

--- JQuery/maze.js
+++ JQuery/maze.js
@@ -1,32 +1,44 @@
-'use strict';
+(function () {
+    "use strict"
+    let timerid = null;
+    let lostGame = false;
+    let illegal = false;;
+    $(function () {
+        $(".boundary").mouseover(youLost);
+        $("#maze").mouseleave(illegalMove);
+        $("#start").mouseover(start);
+        $("#end").mouseover(finish);
+    });
 
-var win = true;
-
-$(document).ready(function() {
-    $("#maze .boundary").mouseover(changeRedColor);
-    $("#start").click(resetAll);
-    $("#maze").mouseleave(changeRedColor);
-    $("#end").mouseover(endTheGame);
-    
-    
-});
-
-function changeRedColor(){
-    if(!win){
-    $("#maze .boundary").addClass("youlose");
-    $("#status").text("You Lose!");
-    win = true;
+    let youLost = function (event) {
+        $(".boundary").addClass("youlose");
+        if (!lostGame) {
+            if (!timerid) {
+                setTimeout(function () {
+                    alert("You Lost The Game!! Press enter to Start Over.");
+                }, 50)
+            }
+        }
+        clearTimeout(timerid);
+        timerid = null;
+        lostGame = true;
     }
-}
-function resetAll(){
-    win = false;
-    $("#maze .boundary").removeClass("youlose");
-    $("#status").text("Move through maze...");
-}
 
-function endTheGame(){
-    if(!win){
-    win = true;
-    $("#status").text("You Win! :]");
+    let start = function () {
+        $(".boundary").removeClass("youlose");
+        lostGame = false;
+        illegal = false;
     }
-}
+    let finish = function () {
+        if (!lostGame && !illegal) {
+            setTimeout(function () {
+                alert("You won the Game!!!");
+            }, 50)
+        }
+
+    }
+    let illegalMove = function () {
+        illegal = true;
+    }
+})();
+
