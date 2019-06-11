28ff84d8e5dc13ca5cb82ce1ce1878909edfb0d6
zelalemzergaw <50334133+zelalemzergaw@users.noreply.github.com>
2019-06-11 14:27:55 -0500

    servlet

---
+++ Servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<p>Postback received</p>");
+        out.print("</body></html>");
+    }
+}
